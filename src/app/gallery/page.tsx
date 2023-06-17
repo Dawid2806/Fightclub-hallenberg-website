69568ae77b729ff110d0dad97263d0333eb6d237
Dawid2806 <David-kaczmarek@gmx.de>
2023-06-17 11:20:18 +0200

    .

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,36 +1,6 @@
-import { Header } from "@/components/Header/Header";
-import { SectionLayout } from "@/components/sectionLayout/SectionLayout";
-import Head from "next/head";
-import Script from "next/script";
+import { GalleryTempleted } from "@/components/template/GalleryTempleted";
 import React from "react";
 
 export default function Gallery() {
-  return (
-    <div>
-      <Head>
-        <title>Gallery</title>
-      </Head>
-      <Header hasVideo={false} url="/image/young-woman-boxer-training-gym.jpg">
-        <>
-          <h1 className="text-7xl text-center ">unsere</h1>
-          <span className="text-8xl  customInnerShadow text-secondary  ">
-            Gallery
-          </span>
-        </>
-      </Header>
-      <SectionLayout>
-        <Script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></Script>
-        <iframe
-          src="//lightwidget.com/widgets/aa53e8e015f05746b1e44b4e6418a30d.html"
-          className="lightwidget-widget py-4"
-          style={{
-            width: "100%",
-            border: 0,
-            overflow: "hidden",
-            height: "100vh",
-          }}
-        ></iframe>
-      </SectionLayout>
-    </div>
-  );
+  return <GalleryTempleted />;
 }
---
+++ src/components/template/TrainingsTempleted.tsx
@@ -0,0 +1,64 @@
+import { Faq } from "@/components/Faq/Faq";
+import { Header } from "@/components/Header/Header";
+import { HeroHeading } from "@/components/HeroHeading/HeroHeading";
+import { SectionLayout } from "@/components/sectionLayout/SectionLayout";
+import { Vortail } from "@/components/vortaile/Vortail";
+import Head from "next/head";
+import React from "react";
+
+export const TrainingsTempleted = () => {
+  return (
+    <div>
+      <Head>
+        <title>Trainings</title>
+      </Head>
+      <Header hasVideo={true} url="/video/training_header.mp4">
+        <>
+          <h1 className="text-7xl text-center ">unsere</h1>
+          <span className="text-8xl   text-secondary  ">Trainings</span>
+        </>
+      </Header>
+      <SectionLayout sectionID="#about">
+        <HeroHeading
+          hasVideo={true}
+          url="video/mma.mp4"
+          description="Willkommen in der aufregenden Welt des MMA! Mixed Martial Arts, oder MMA, ist eine Vollkontaktsportart, die eine Vielzahl von Kampftechniken und -fähigkeiten aus verschiedenen Kampfsportarten und Kampfkünsten kombiniert. Von Boxen über Jiu-Jitsu bis hin zu Wrestling und Muay Thai, MMA bietet eine einzigartige Plattform für Athleten, um ihre Fähigkeiten in einem dynamischen und herausfordernden Umfeld zu demonstrieren. MMA ist mehr als nur ein Sport, es ist eine Lebenseinstellung, die Disziplin, Ausdauer und ständige Verbesserung erfordert
+          "
+          isReverse={false}
+          title="Mixed Martial Arts
+          "
+        ></HeroHeading>
+        <HeroHeading
+          hasVideo={true}
+          url="video/thaiboxen.mp4"
+          description="Tauchen Sie ein in die Welt des Thaiboxens, auch bekannt als Muay Thai. Dieser antike Kampfsport, der seine Wurzeln in Thailand hat, ist bekannt für seine kraftvollen Schläge und Tritte, sowie für den Einsatz von Ellenbogen und Knien. Thaiboxen ist nicht nur ein effektiver Selbstverteidigungssport, sondern auch eine hervorragende Möglichkeit, Fitness und Ausdauer zu verbessern. Es fördert Disziplin, Respekt und Selbstvertrauen, und ist eine spannende und herausfordernde Art, in Form zu bleiben.
+          "
+          isReverse={true}
+          title="Thaiboxing"
+        ></HeroHeading>
+        <HeroHeading
+          hasVideo={true}
+          url="video/womentainigns.mp4"
+          description="Kampfkunst ist nicht nur für Männer. Immer mehr Frauen entdecken die Vorteile des Kampfkunsttrainings. Es bietet nicht nur eine effektive Methode zur Selbstverteidigung, sondern auch eine Möglichkeit, körperliche Fitness, Selbstvertrauen und geistige Stärke zu verbessern. Egal, ob Sie sich für Karate, Jiu-Jitsu, Kickboxen oder eine andere Kampfkunst entscheiden, Sie werden eine Gemeinschaft von gleichgesinnten Frauen finden, die sich gegenseitig unterstützen und ermutigen.
+          "
+          isReverse={false}
+          title="Ladytraining"
+        ></HeroHeading>
+        <HeroHeading
+          hasVideo={true}
+          url="video/child-trainigns.mp4"
+          description="Kampfkunsttraining ist eine hervorragende Möglichkeit für Kinder, ihre körperliche Fitness zu verbessern, Selbstvertrauen aufzubauen und wichtige Werte wie Respekt, Disziplin und Durchhaltevermögen zu lernen. Durch das Training lernen sie, wie sie sich selbst verteidigen können, und entwickeln gleichzeitig eine positive Einstellung und einen starken Charakter. Egal, ob Ihr Kind sich für Karate, Judo, Taekwondo oder eine andere Kampfkunst interessiert, es wird eine sichere und unterstützende Umgebung finden, in der es wachsen und lernen kann.
+          "
+          isReverse={true}
+          title="Kindertraining"
+        ></HeroHeading>
+      </SectionLayout>
+      <SectionLayout sectionTitle="Faq" sectionID="#faq">
+        <Faq />
+      </SectionLayout>
+      <span></span>
+      <Vortail url="/video/Selbstverteidigung.mp4" title="Selbstverteidigung" />
+      <Vortail url="/video/fit.mp4" title="Fit zu sein" />
+    </div>
+  );
+};
