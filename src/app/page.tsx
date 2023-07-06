4513c9dca3cf5d762339ae48ad19a1aa12882554
Dawid2806 <David-kaczmarek@gmx.de>
2023-07-06 16:05:42 +0200

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import { Home } from "@/components/template/Home";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function HomePage() {
   return (
     <>
       <Head>
         <title>FightClub-Hallenberg</title>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-939WT6M10E" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-939WT6M10E');
+        `}
+        </Script>
       </Head>
       <Home />
     </>
