1348dbdb85d59186c5ff94a1a487cd65f58b37dd
Dawid Kaczmarek <David-kaczmarek@gmx.de>
2023-11-02 21:20:14 +0100

    Update TrainingDay.tsx

--- src/components/TrainingZeitList/TrainingDay.tsx
+++ src/components/TrainingZeitList/TrainingDay.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 interface TrainingDayProps {
   day: string;
-  children: ReactNode;
+  children?: ReactNode;
 }
 
 export const TrainingDay = ({ day, children }: TrainingDayProps) => {
