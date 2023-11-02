4d632bd63c864489a4125f79934f552d88a1e2cb
Dawid Kaczmarek <David-kaczmarek@gmx.de>
2023-11-02 21:13:07 +0100

    Update TrainingsTable.tsx

--- src/components/TrainingZeitList/TrainingsTable.tsx
+++ src/components/TrainingZeitList/TrainingsTable.tsx
@@ -8,28 +8,24 @@ export const TrainingsTable = () => {
     <div>
       <div className="flex flex-col items-center lg:items-start lg:flex-row mx-auto px-4 lg:justify-between  my-20 ">
         <TrainingDay day="montag">
-          <Training time="17:00-18:00" training="Kinder-Training" />
+          <Training time="16:00-17:00" training="Kinder-Training" />
+            <Training time="17:00-18:15" training="Lady-Training" />
           <Training time="18:30-19:45" training="Thaiboxen" />
           <Training time="20:00-21:15" training="MMA" />
         </TrainingDay>
-        <TrainingDay day="dienstag">
-          <Training time="17:00-18:15" training="Lady-Training" />
-        </TrainingDay>
+        
         <TrainingDay day="mittwoch">
-          <Training time="17:00-18:00" training="Kinder-Training" />
-          <Training time="18:30-19:45" training="Thaiboxen" />
-          <Training time="20:00-21:15" training="MMA" />
-        </TrainingDay>
-        <TrainingDay day="donnerstag">
+          <Training time="16:00-17:00" training="Kinder-Training" />
           <Training time="17:00-18:15" training="Lady-Training" />
-        </TrainingDay>
-        <TrainingDay day="freitag">
           <Training time="18:30-19:45" training="Thaiboxen" />
           <Training time="20:00-21:15" training="MMA" />
         </TrainingDay>
-        <TrainingDay day="samstag">
-          <Training time="10:30-11:30" training="Kinder-Training" />
-          <Training time="12:00-13:15" training="Lady-Training" />
+        
+        <TrainingDay day="freitag">
+                    <Training time="16:00-17:00" training="Kinder-Training" />
+                    <Training time="17:00-18:15" training="Lady-Training" />
+          <Training time="18:30-19:45" training="Thaiboxen" />
+          <Training time="20:00-21:15" training="MMA" />
         </TrainingDay>
         <TrainingDay day="sonntag">
           <Training time="nach vereinbarung" training="Offene Matte" />
